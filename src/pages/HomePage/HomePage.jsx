2f2bfa06b4aba1acb6180ddcca90024f1770d398
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-07-25 13:53:03 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,14 @@
 import css from "./HomePage.module.css";
 
 export default function HomePage() {
-  return (
-    <div className={css.home}>
-      <h1 className={css.title}>Welcome to PhoneBook! </h1>
-      <p className={css.text}>
-        Save your contacts, quickly find phone numbers, and manage your address
-        book. <br />
-        Thank you for choosing our app 😊
-      </p>
-    </div>
-  );
+    return (
+        <div className={css.home}>
+            <h1 className={css.title}>Welcome to PhoneBook! </h1>
+            <p className={css.text}>
+                Save your contacts, quickly find phone numbers, and manage your
+                address book. <br />
+                Thank you for choosing our app 😊
+            </p>
+        </div>
+    );
 }
